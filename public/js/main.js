ac49c0abe6f4105d5af51bf0c2dd29ea34ac1636
Josey9087 <88466226+Josey9087@users.noreply.github.com>
2021-12-11 16:17:16 -0700

    connecting front and back user routes and buttons

--- public/js/main.js
+++ public/js/main.js
@@ -1,58 +1,108 @@
 // Search for plants
-// const searchInput = document.querySelector('#search-input');
-// const searchButton = document.querySelector('.submit');
+const searchInput = document.querySelector('#search-input');
+const searchButton = document.querySelector('.submit');
 
-// const getSearch = async (e) => {
-//     e.preventDefault();
-//     console.log(searchInput.value)
-//     try {
-//         window.location.href = `/home/search/${searchInput.value}`
-//     } catch (err) {
-//         console.log(err);
-//     }
-// }
-
-
-// searchButton.addEventListener('click', getSearch);
-
-// Create a user
-
-var firstnameinput = document.getElementById('firstname');
-var lastnameinput = document.getElementById('lastname');
-var emailinput = document.getElementById('email');
-var usernameinput = document.getElementById('username');
-var passwordinput = document.getElementById('password');
-
-const userbutton = document.querySelector('.user');
-const newuser = async (e) => {
+const getSearch = async (e) => {
     e.preventDefault();
-    userdata = {"firstname": firstnameinput.value, 
-                    "lastname": lastnameinput.value,
-                    "email": emailinput.value,
-                    "username": usernameinput.value,
-                    "password": passwordinput.value}
-                    console.log(JSON.stringify(userdata))
-                    console.log(userdata)
+    console.log(searchInput.value)
     try {
-        fetch('http://localhost:3001/user', {
-  method: 'POST', 
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  body: JSON.stringify(userdata),
-})
-.then(response => response.json())
-.then(data => {
-  console.log('Success:', data);
-  window.location.href = `/home`
-})
-.catch((error) => {
-  console.error('Error:', error);
-});
+        window.location.href = `/search/${searchInput.value}`
     } catch (err) {
         console.log(err);
     }
 }
 
 
-userbutton.addEventListener('click', newuser)
+searchButton.addEventListener('click', getSearch);
+
+// Create a user
+
+// var emailinput = document.getElementById('email');
+// var usernameinput = document.getElementById('username');
+// var passwordinput = document.getElementById('password');
+
+// const userbutton = document.querySelector('.user');
+// const newuser = async (e) => {
+//     e.preventDefault();
+//     userdata = {"email": emailinput.value,
+//                 "username": usernameinput.value,
+//                 "password": passwordinput.value}
+//                     console.log(JSON.stringify(userdata))
+//                     console.log(userdata)
+//     try {
+//         fetch('http://localhost:3001/login', {
+//   method: 'POST', 
+//   headers: {
+//     'Content-Type': 'application/json',
+//   },
+//   body: JSON.stringify(userdata),
+// })
+// .then(response => response.json())
+// .then(data => {
+//   console.log('Success:', data);
+//   window.location.href = `/home`
+// })
+// .catch((error) => {
+//   console.error('Error:', error);
+// });
+//     } catch (err) {
+//         console.log(err);
+//     }
+// }
+
+// userbutton.addEventListener('click', newuser)
+
+
+const loginFormHandler = async (event) => {
+  event.preventDefault();
+
+  // Collect values from the login form
+  const email = document.querySelector('#email').value.trim();
+  const password = document.querySelector('#password').value.trim();
+
+  if (email && password) {
+    // Send a POST request to the API endpoint
+    const response = await fetch('http://localhost:3001/login', {
+      method: 'POST',
+      body: JSON.stringify({ email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      // If successful, redirect the browser to the profile page
+      document.location.replace('/profile');
+    } else {
+      alert(response.statusText);
+    }
+  }
+};
+
+const signupFormHandler = async (event) => {
+  event.preventDefault();
+
+  const name = document.querySelector('#name-signup').value.trim();
+  const email = document.querySelector('#email-signup').value.trim();
+  const password = document.querySelector('#password-signup').value.trim();
+
+  if (name && email && password) {
+    const response = await fetch('http://localhost:3001/users', {
+      method: 'POST',
+      body: JSON.stringify({ name, email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      document.location.replace('/profile');
+    } else {
+      alert(response.statusText);
+    }
+  }
+};
+
+document
+  .querySelector('.user')
+  .addEventListener('submit', loginFormHandler);
+
+document
+  .querySelector('.signup-form')
+  .addEventListener('submit', signupFormHandler);
