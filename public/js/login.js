276f595676caf7eb084de01fd6d1490f9222a05c
Josey9087 <88466226+Josey9087@users.noreply.github.com>
2021-12-11 18:40:51 -0700

    signup route issues

--- public/js/login.js
+++ public/js/login.js
@@ -25,29 +25,40 @@ const loginFormHandler = async (event) => {
 const signupFormHandler = async (event) => {
   event.preventDefault();
 
-  const name = document.querySelector('#name-signup').value.trim();
+  const username = document.querySelector('#name-signup').value.trim();
   const email = document.querySelector('#email-signup').value.trim();
   const password = document.querySelector('#password-signup').value.trim();
 
-  if (name && email && password) {
-    const response = await fetch('http://localhost:3001/users', {
-      method: 'POST',
-      body: JSON.stringify({ name, email, password }),
-      headers: { 'Content-Type': 'application/json' },
-    });
-
-    if (response.ok) {
-      document.location.replace('/profile');
-    } else {
-      alert(response.statusText);
+    userdata = {"email": email,
+                "username": username,
+                "password": password}
+                    console.log(JSON.stringify(userdata))
+                    console.log(userdata)
+    try {
+        fetch('http://localhost:3001/user', {
+  method: 'POST', 
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify(userdata),
+})
+.then(response => response.json())
+.then(data => {
+  console.log('Success:', data);
+  window.location.href = `/home`
+})
+.catch((error) => {
+  console.error('Error:', error);
+});
+    } catch (err) {
+        console.log(err);
     }
-  }
-};
+}
 
 document
   .querySelector('.user')
   .addEventListener('submit', loginFormHandler);
 
 document
-  .querySelector('.signup-form')
+  .querySelector('.signup')
   .addEventListener('submit', signupFormHandler);
