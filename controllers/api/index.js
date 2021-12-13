e8aa18621906bf3ecec13ea9cddbf329b676a31f
mado <madelineadonley@gmail.com>
2021-12-13 13:29:26 -0700

    added cloudinary upload image functionality

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
 const plantRoutes = require('./plantRoutes');
 const postRoutes = require('./postRoutes');
+const uploadRoutes = require('./uploadRoutes')
 
 router.use('/plants', plantRoutes);
 router.use('/posts', postRoutes);
-
+router.use('/', uploadRoutes);
 
 module.exports = router;
--- controllers/api/uploadRoutes.js
+++ controllers/api/uploadRoutes.js
@@ -1,6 +1,8 @@
+const router = require('express').Router();
 const multer  = require('multer');
 const upload = multer({ dest: 'uploads/' });
 const cloudinary = require('cloudinary').v2;
+const { unlink } = require('fs');
 
 require('dotenv').config();
 
@@ -10,3 +12,38 @@ cloudinary.config({
     api_secret: process.env.API_SECRET,
     secure: true
 });
+
+router.post('/upload', upload.single('myFile'), async ({ body, file }, res) => {
+    // console.log(req.body); // req.body will hold the text fields appended to your formData object
+    // console.log(req.file); // req.file is the name of your file in the form above, here 'uploaded_file'
+    const { filename, destination } = file;
+    const pathToFile = `${destination}/${filename}`;
+    try {
+        const { url } = await cloudinary.uploader
+            .upload(pathToFile, {
+                resource_type: "image",
+                public_id: 'posts',
+            },
+                (error) => {
+                    if (error) console.error(error)
+                }
+            );
+
+        const httpsUrl = `https://${url.split('://')[1]}` // convert http to https
+
+        /**
+         * the resourse is now hosted, send back the url to user.
+         * if you'd like to save the url to a db, do this here
+         */
+
+        res.json({ ...body, url: httpsUrl }); // respond with url and  inital request body
+        unlink(pathToFile, (error) => {
+            if (error) console.error(error)
+        });
+    } catch (error) {
+        console.error(error);
+        res.json({ msg: 'failure' });
+    }
+});
+
+module.exports = router;
